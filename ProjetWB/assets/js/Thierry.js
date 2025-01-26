7355776bc98fab430cfbb59a5d56af9e49ae0fe7
Tedioo <Tedio111@outlook.fr>
2025-01-26 03:50:18 +0100

    Boxe2 ajustement + Banner ajout lien icone
    Boxe 2 :
    Ajustement des icones et des textes
    Ajout de la newsletter + Message de confirmation et d'erreur
    
    Banner :
    Ajout de liens sur les icones de magasins

--- ProjetWB/assets/js/Thierry.js
+++ ProjetWB/assets/js/Thierry.js
@@ -1,3 +1,5 @@
+/************* DEBUT Gestion de la bannière *************/
+
 let scrollContainer = document.querySelector('.bannière');  // Récupère le conteneur de la bannière
 let suivant = document.getElementById('next');              // Récupère le bouton suivant
 let precedent = document.getElementById('previous');        // Récupère le bouton précédent
@@ -6,16 +8,50 @@ scrollContainer.addEventListener("wheel", (evt) => {        // Ajoute un écoute
     evt.preventDefault();                                   // Empêche le comportement par défaut de la molette de la souris
     scrollContainer.scrollLeft += evt.deltaY;               // Fait défiler le conteneur de la bannière horizontalement
     scrollContainer.style.scrollBehavior = "smooth";        // Ajoute un effet de défilement fluide
-    let scrollDistance = 585;                               // Définit la distance de défilement
+    let scrollDistance = 975;                               // Définit la distance de défilement
     scrollContainer.scrollLeft += evt.deltaY > 0 ? scrollDistance : -scrollDistance;    // Fait défiler le conteneur de la bannière horizontalement
 });
 
 suivant.addEventListener("click", () => {                   // Ajoute un écouteur d'événement pour le clic sur le bouton suivant
     scrollContainer.style.scrollBehavior = "smooth";        // Ajoute un effet de défilement fluide
-    scrollContainer.scrollLeft += 585;                      // Fait défiler le conteneur de la bannière horizontalement
+    scrollContainer.scrollLeft += 975;                      // Fait défiler le conteneur de la bannière horizontalement selon la largeur de l'image 150+25 = 175
 });
 
 precedent.addEventListener("click", () => {                 // Ajoute un écouteur d'événement pour le clic sur le bouton précédent
     scrollContainer.style.scrollBehavior = "smooth";        // Ajoute un effet de défilement fluide
-    scrollContainer.scrollLeft -= 585;                      // Fait défiler le conteneur de la bannière horizontalement
+    scrollContainer.scrollLeft -= 975;                      // Fait défiler le conteneur de la bannière horizontalement selon la largeur de l'image 150+25 = 175
 });
+
+
+/************* FIN Gestion de la bannière *************/
+
+
+
+/************* DEBUT Barre de mail *************/
+
+let souscrire = document.getElementById('souscrire');   // Récupère le bouton souscrire
+const confirmationMessage = document.getElementById("confirmationMessage");
+
+function estUneAdresseMail(mail) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  // Expression pour vérifier si c'est un email
+    if (regex.test(mail.value)) {               // Si l'expression est un vrai email 
+        console.log("Adresse email valide");    // Affiche dans la console un message de confirmation d'inscription à la newsletter
+        return true;                            // retourne vraie
+    } else {                                    // Sinon
+        console.log("Adresse email invalide");  // Affiche dans la console un message d'erreur d'inscription à la newsletter
+        return false;                          // Retourne faux
+        }
+    }
+
+    souscrire.addEventListener("click", () => {             // Ajoute un écouteur d'événement pour le clic sur le bouton souscrire
+        const mail = document.getElementById('emailInput'); // Récupère l'élément mail
+
+        if (estUneAdresseMail(mail)) {                      // Vérifie si l'adresse mail est valide
+        confirmationMessage.classList.add("show");      // Affiche le message de confirmation
+        errorMessage.classList.remove("show");          // Cache le message d'erreur
+        mail.value = "";                                // Réinitialise le champ email
+        } else {                                            // Sinon
+        errorMessage.classList.add("show");             // Affiche le message d'erreur
+        confirmationMessage.classList.remove("show");   // Cache le message de confirmation
+        }
+    });
