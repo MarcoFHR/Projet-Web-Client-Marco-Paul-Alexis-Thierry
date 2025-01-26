0d7faf55ed5a8123a8b9133afeec40ace6e91060
MarcoFHR <marco.defreitashead-rapson@etu.univ-tours.fr>
2025-01-26 20:12:33 +0100

    Dropdown done

--- ProjetWB/assets/js/Marco.js
+++ ProjetWB/assets/js/Marco.js
@@ -45,10 +45,12 @@ function changeBGColor(elem, color){
 
 function toggleBloc(elem, lock){
   if(lock.value){
-    elem.style.display = "none";
+    //elem.style.display = "none";
+    elem.style.maxHeight = elem.parentElement.offsetHeight + "px";
   }
   else{
-    elem.style.display = "block";
+    //elem.style.display = "block";
+    elem.style.maxHeight = elem.scrollHeight + "px";
   }
   lock.value = !lock.value;
 }
@@ -89,14 +91,14 @@ onMouseTransform(fl2, "translate(0.3rem, 0)", "translate(0, 0)");
 onMouseTransform(ff1, "translate(0, -0.3rem)", "translate(0, 0)");
 onMouseTransform(ff2, "translate(0, -0.3rem)", "translate(0, 0)");
 */
-fl1.onmouseover = function() {changePos(fl1,"0.3rem","0"); changeBGColor(fl1, "#fc6a03");};
-fl2.onmouseover = function() {changePos(fl2,"0.3rem","0"); changeBGColor(fl2, "#fc6a03");};
+fl1.onmouseover = function() {changePos(fl1,"0.3rem","0"); changeBGColor(fl1, "#FF0000");};
+fl2.onmouseover = function() {changePos(fl2,"0.3rem","0"); changeBGColor(fl2, "#FF0000");};
 
 fl1.onmouseout = function() {changePos(fl1,"0","0"); changeBGColor(fl1, "");};
 fl2.onmouseout = function() {changePos(fl2,"0","0"); changeBGColor(fl2, "");};
 
-ff1.onmouseover = function() {changePos(ff1,"0","-0.3rem"); changeBGColor(ff1, "#fc6a03");};
-ff2.onmouseover = function() {changePos(ff2,"0","-0.3rem"); changeBGColor(ff2, "#fc6a03");};
+ff1.onmouseover = function() {changePos(ff1,"0","-0.3rem"); changeBGColor(ff1, "#FF0000");};
+ff2.onmouseover = function() {changePos(ff2,"0","-0.3rem"); changeBGColor(ff2, "#FF0000");};
 
 ff1.onmouseout = function() {changePos(ff1,"0","0"); changeBGColor(ff1, "");};
 ff2.onmouseout = function() {changePos(ff2,"0","0"); changeBGColor(ff2, "");};
